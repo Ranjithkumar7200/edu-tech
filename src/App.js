4c667b67f4ff9eead2c91adacf0b353607dd36d6
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2024-09-27 16:27:17 +0530

    added courses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Courses from "./pages/courses/Courses";
 import EduTechNavbar from "./pages/Navbar/EduTechNavbar";
 
 function App() {
   return (
     <div style={{ backgroundColor: "black" }}>
       <EduTechNavbar />
+      <Courses />
     </div>
   );
 }
